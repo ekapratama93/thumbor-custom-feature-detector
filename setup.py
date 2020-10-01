2eab263669f428676438f804027b97e66f4c5f11
ekapratama93 <ekapratama93@gmail.com>
2020-10-01 15:04:41 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ setup(
     author="ekapratama93",
     description=("Thumbor custom algorithm for feature detection"),
     license="MIT",
-    keywords="thumbor mongodb mongo",
+    keywords="thumbor feature detector opencv",
     url="https://github.com/ekapratama93/thumbor-custom-feature-detector",
     packages=find_packages(include=[
-        'thumbor_custom.detectprs.feature_detector',
+        'thumbor_custom.detectors.feature_detector',
     ]),
     long_description=long_description,
     classifiers=[
