9c0efd65d425b36e94970bbe943e75265e997843
ekapratama93 <ekapratama93@gmail.com>
2020-10-01 13:00:38 +0700

    init code

---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = 'Thumbor custom algorithm for feature detection'
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="thumbor_custom",
+    version="0.0.1",
+    author="ekapratama93",
+    description=("Thumbor custom algorithm for feature detection"),
+    license="MIT",
+    keywords="thumbor mongodb mongo",
+    url="https://github.com/ekapratama93/thumbor-custom-feature-detector",
+    packages=find_packages(include=[
+        'thumbor_custom.detectprs.feature_detector',
+    ]),
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Multimedia :: Graphics :: Presentation",
+    ],
+    install_requires=[
+        'thumbor>=7.0.0a5,<8.0.0',
+        "opencv-python-headless==4.*,>=4.2.0",
+    ]
+)
---
+++ thumbor_custom/detectors/feature_detector/__init__.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2020 Eka Cahya Pratama <ekapratama93@gmail.com>
+
+import cv2
+import numpy as np
+
+from thumbor.detectors import BaseDetector
+from thumbor.point import FocalPoint
+from thumbor.utils import logger
+
+
+class Detector(BaseDetector):
+    async def detect(self):
+        engine = self.context.modules.engine
+        try:
+            img = np.copy(
+                engine.convert_to_grayscale(update_image=False, alpha=False)
+            )
+        except Exception as error:
+            logger.exception(error)
+            logger.warning(
+                "Error during feature detection; skipping to next detector")
+            return await self.next()
+
+        mode = 'ORB'
+        if self.context.config.CUSTOM_FEATURE_DETECTOR_ALGORITHM:
+            mode = self.context.config.CUSTOM_FEATURE_DETECTOR_ALGORITHM\
+                    .upper()
+
+        max_feature = 20
+        if self.context.config.CUSTOM_FEATURE_DETECTOR_MAX_FEATURE:
+            max_feature = int(
+                self.context.config.CUSTOM_FEATURE_DETECTOR_MAX_FEATURE
+            )
+
+        threshold = 100
+        if self.context.config.CUSTOM_FEATURE_DETECTOR_THRESHOLD:
+            threshold = int(
+                self.context.config.CUSTOM_FEATURE_DETECTOR_THRESHOLD
+            )
+
+        # default ORB detector
+        detector = None
+        if mode == 'FAST':
+            detector = cv2.FastFeatureDetector_create(threshold=threshold)
+        elif mode == 'AGAST':
+            detector = cv2.AgastFeatureDetector_create(threshold=threshold)
+        elif mode == 'AKAZE':
+            detector = cv2.AKAZE_create(threshold=0.008)
+        elif mode == 'BRISK':
+            detector = cv2.BRISK_create(thresh=threshold)
+        elif mode == 'SIFT':
+            detector = cv2.SIFT_create(nfeatures=max_feature)
+        else:
+            detector = cv2.ORB_create(nfeatures=max_feature)
+
+        try:
+            keypoints = detector.detect(img)
+        except Exception as error:
+            logger.exception(error)
+            logger.warning(
+                "Error during feature detection; no keypoint detected")
+            return await self.next()
+
+        points = cv2.KeyPoint_convert(keypoints)
+
+        if points is not None:
+            for point in points:
+                x_pos, y_pos = point
+                self.context.request.focal_points.append(
+                    FocalPoint(x_pos.item(), y_pos.item(), 1)
+                )
+            return
+
+        await self.next()
